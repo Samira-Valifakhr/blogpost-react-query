f23d6974449af385004b0864ba4331a357d20862
siavashsajady <hubgitpro@gmail.com>
2022-03-18 17:51:19 +0330

    first commit

---
+++ src/components/FindPosts.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+
+async function fetchPosts(title) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  return response.json();
+}
+
+export default function(post) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { data, isError, error, isLoading } = useQuery(
+    ['post', post.title],
+    () => fetchPosts(post.title)
+  );
+
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='search...'
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {data &&
+        data
+          .filter((post) => {
+            if (searchTerm == '') {
+              return post;
+            } else if (
+              post.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return post;
+            }
+          })
+          .map((post, key) => {
+            return (
+              <div key={post.userId}>
+                <p>{post.title}</p>
+              </div>
+            );
+          })}
+    </div>
+  );
+}
